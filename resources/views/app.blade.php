8f696108889df59742c04ee4de445d3d070a1178
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2024-02-16 12:49:28 +0600

    update readme file

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,7 +16,6 @@
 @inertia
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-{{--<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/go.min.js"></script>--}}
 
 {{--<script>hljs.highlightAll();</script>--}}
 </body>
